713f8a6266c2140916a6690e3ce4f8f6600fa8c3
marf33 <miguel.figueiredo96@gmail.com>
2024-07-16 17:48:40 +0100

    Inicializar repositório com exportação do SVN

---
+++ teste/deploy.ps1
@@ -0,0 +1,11 @@
+#!/usr/bin/env pwsh
+
+Write-Output "Iniciando a implementação..."
+
+# Puxar as últimas alterações
+git pull origin main
+
+# Executar comandos de construção ou implementação
+# Exemplo: npm install && npm run build
+
+Write-Output "Implementação concluída."
